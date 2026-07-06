fee24e94fab52363e098e31b540d7d81f0a5e65e
Xavier Brooke <xavier.brooke.official@gmail.com>
2026-07-07 02:36:46 +0530

    added _05_REST

---
+++ web_dev/_05_backend/_05_REST/app.js
@@ -0,0 +1,20 @@
+const express = require("express") ;
+const methodOverride = require("method-override") ;
+const path = require("path") ;
+const data = require("./public/data/data") ;
+
+const app = express() ;
+const port = 3000 ;
+
+app.set("view engine", "ejs") ;
+app.set("views", path.join(__dirname, "/views")) ;
+
+app.use(express.static(path.join(__dirname, "public"))) ;
+
+app.get("/filmyWorld", (req, res) => {
+    res.render("home.ejs", {data}) ;
+})
+
+app.listen(port, () => {
+    console.log(`App is listening at port ${port}`) ;
+})
