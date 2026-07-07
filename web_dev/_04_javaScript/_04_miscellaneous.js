b5e62780fb1327c8c78b0c1d3f94de3af5576a87
Xavier Brooke <xavier.brooke.official@gmail.com>
2026-07-08 03:15:14 +0530

    practice : miscellaneous topics of js

---
+++ web_dev/_04_javaScript/_04_miscellaneous.js
@@ -0,0 +1,44 @@
+// const sum = (a, b) => {
+//     return a + b ;
+// }
+// console.log(sum(1,1)) ;
+
+// const cube = (a) => {
+//     return a**3 ;
+// }
+// console.log(cube(3)) ;
+
+// setTimeout(() => {
+//     console.log("Hello World") ;
+// }, 1000) ;
+
+// function greet() {
+//     console.log("Hello World!") ;
+// }
+// setTimeout(greet(), 100) ;
+
+// let id1 = setInterval(() => {
+//     console.log(`Hello World!`) ;
+// }, 1000) ;
+// setTimeout(() => {
+//     clearInterval(id1)
+// }, 10000) ;
+
+// let greet = function() {
+//     console.log(`Hello World!!`) ;
+// }
+// let id2 = setInterval(() => greet(), 100) ;
+// setTimeout(() => {
+//     clearInterval(id2)
+// }, 10000) ;
+
+// const student = {
+//     name : "aman",
+//     marks : 78,
+//     getName() {
+//         return this.name ;
+//     },
+//     getMarks : () => {
+//         console.log(this) ;
+//     }
+// }
