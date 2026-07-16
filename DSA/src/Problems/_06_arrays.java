cad042b80e204eeae709a5f3f82d307ee89077dc
Xavier Brooke <xavier.brooke.official@gmail.com>
2026-07-16 15:20:49 +0530

    practice : ArrayList

--- DSA/src/Problems/_06_arrays.java
+++ DSA/src/Problems/_06_arrays.java
@@ -35,15 +35,60 @@ public class _06_arrays {
         return ans ;
     }
 
+    /*
+    * Problem 2 :-
+    * WAF to sort 0s, 1s and 2s*/
+    public static void sort_0s_1s_2s(int[] arr) {
+        int i = 1 ;
+        int j = arr.length-1 ;
+        int p = 1 ;
+        int q = arr.length-1 ;
+        while((i < j) || (p < q)) {
+            if(i < j) {
+                if(((arr[j] == 0) || (arr[j] == 1)) && (arr[i] == 2)) {
+                    int temp = arr[j] ;
+                    arr[j] = arr[i] ;
+                    arr[i] = temp ;
+                    i++ ;
+                    j-- ;
+                } else if(arr[j] == 2) {
+                    j-- ;
+                } else if((arr[i] == 0) || (arr[i] == 1)) {
+                    i++ ;
+                }
+            }
+
+            if(p < q) {
+                if((arr[p] == 1) && (arr[q] == 0)) {
+                    int temp = arr[p] ;
+                    arr[p] = arr[q] ;
+                    arr[q] = temp ;
+                    p++ ;
+                    q-- ;
+                } else if(arr[p] == 0) {
+                    p++ ;
+                } else if((arr[q] == 1) || (arr[q] == 2)) {
+                    q-- ;
+                }
+            }
+        }
+    }
+
+
+
     // main function
     public static void main(String[] args) {
+
+        // Test Case for Question 2 :-
+        int[] arr = {0, 1, 1, 1, 2, 0, 0, 0, 2} ;
+        sort_0s_1s_2s(arr);
 
         // Test Case for Question 1 :-
 //        int[] a = {1, 3, 5, 7, 9} ;
 //        int[] b = {2, 4, 6, 8, 10} ;
 //        int[] c = mergeArr(a, b) ;
-//        for(int ele : c) {
-//            System.out.print(ele + " ");
-//        }
+        for(int ele : arr) {
+            System.out.print(ele + " ");
+        }
     }
 }
