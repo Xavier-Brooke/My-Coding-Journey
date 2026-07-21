fd8a70d889b66ffd6aea032e86b51cc8c7bbfa26
Xavier Brooke <xavier.brooke.official@gmail.com>
2026-07-22 02:57:54 +0530

    practice : state in react

---
+++ web_dev/_05_backend/_11_react.js/lecture_2/React-State/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Counter from './Counter.jsx' ;
+import LikeButton from './LikeButton.jsx';
+
+function App() {
+  return (
+    <div>
+        <LikeButton/>
+    </div>
+  )
+}
+
+export default App
